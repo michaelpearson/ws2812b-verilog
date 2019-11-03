3a83845a6b9c5f2440f5477f0cb4b9fef5a62a25
Michael <zl2mjp@gmail.com>
2019-11-03 18:14:35 +1300

    Multipule LEDs on a single channel

--- ws2812b-verilog.srcs/sources_1/new/top.v
+++ ws2812b-verilog.srcs/sources_1/new/top.v
@@ -24,29 +24,26 @@
 module top(
     input clk,
     input SW [15:0],
+    input BTNL, // Latch Data
+    input BTNR, // Start TX
     output LED [15:0],
     output OUT [5:0]
 );
+  wire [7:0] r, g, b, address;
+  wire latch, start_tx;
 
-wire [7:0] r, g, b;
-reg [19:0] counter;
-
-generate
-  for (genvar i = 0; i < 8; i = i + 1) begin
-    assign r[i] = SW[i];
-    assign g[i] = SW[8 + i];
-    assign b[i] = SW[i];
-  end
-endgenerate
-
-
-reg latch_data = 0;
-reg previous_switch = 0;
-
-always@(posedge clk) begin
-  counter <= counter + 1;
-end
-
-ws2812b led(clk, r, g, b, counter[19], OUT[0]);
+  assign latch = BTNL;
+  assign start_tx = BTNR;
+  generate
+    for (genvar i = 0; i < 8; i = i + 1) begin
+      assign r[i] = SW[i];
+      assign g[i] = SW[i];
+      assign b[i] = SW[i];
+      assign address[i] = SW[i + 8];
+    end
+  endgenerate
+    
+  defparam led.LED_COUNT = 28;
+  ws2812b led(clk, address, r, g, b, latch, start_tx, OUT[0]);
 
 endmodule
--- ws2812b-verilog.srcs/sources_1/new/ws2812b.v
+++ ws2812b-verilog.srcs/sources_1/new/ws2812b.v
@@ -19,44 +19,41 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-module ws2812b(
-    input clk,
-    input wire [7:0] r,
-    input wire [7:0] g,
-    input wire [7:0] b,
-    input latch,
-    output data
+module ws2812b #(parameter LED_COUNT = 1) (
+  input wire clk,
+  input wire [7:0] address,
+  input wire [7:0] r,
+  input wire [7:0] g,
+  input wire [7:0] b,
+  input wire latch,
+  input wire start_tx,
+  output wire data
 );
-
-reg [31:0] color = 0;
-reg [11:0] counter = 0;
-
-reg tx = 0;
-reg data_ready = 0;
-reg data_read = 0;
-
-always@(posedge latch or posedge data_read) begin
-  if (latch) begin
-    data_ready <= 1;
-    color <= {g[7:0], r[7:0], b[7:0], 8'h00};
-  end else if (data_read)
-    data_ready <= 0;
-end
-
-always@(posedge clk) begin
-  if (tx) begin
-    data_read <= 0;
-    counter = counter + 1;
-    if (counter == 0) begin
-      tx <= 0;
+  localparam COLOUR_WIDTH = LED_COUNT * 24;
+  
+  reg [COLOUR_WIDTH:0] colours = 0;
+  reg [6:0] drive_counter;
+  reg [13:0] bit_counter;
+  reg start_tx_previous;
+  
+  always@(posedge latch) begin
+    if (address < LED_COUNT) begin
+      colours[(24 * (LED_COUNT - address) - 1)-:23] = {g, r, b};
     end
-  end else if (data_ready) begin
-    data_read <= 1;
-    tx <= 1;
   end
-end
-
-// Bits are arranged in memory MSB -> LSB. Incrementing counter will write bits LSB -> MSB. Need to reverse counter.
-assign data = tx && (counter[11:7] < 24) && (color[31 - counter[11:7]] ? (counter[6:0] < 100) : (counter[6:0] < 10));
+  
+  always@(posedge clk) begin
+    if (start_tx && start_tx != start_tx_previous) begin
+      bit_counter <= COLOUR_WIDTH - 1;
+      drive_counter <= 0;
+    end else if (bit_counter < COLOUR_WIDTH) begin
+      drive_counter = drive_counter + 1;
+      bit_counter = bit_counter - (drive_counter == 0);
+    end
+    
+    start_tx_previous = start_tx;
+  end
+  
+  assign data = (bit_counter < COLOUR_WIDTH) && (colours[bit_counter] ? (drive_counter < 100) : (drive_counter < 10));
 
 endmodule
